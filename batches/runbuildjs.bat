19716a7574c01b9902734f4bbb3ccd40420baf59
Randall Ratsch <rratsch@gmail.com>
2024-09-21 20:39:03 -0400

    First commit

---
+++ batches/runbuildjs.bat
@@ -0,0 +1,13 @@
+cls
+set options=doc srcjs testjs
+set runablefolder=../../vxlisp/dist
+rem set runablefolder=../../vxlisp/src
+set currentfolder=%cd%
+set srcfolder=%currentfolder%/../src
+set runable=vxlisp_win64.exe
+rem set runable=go run .
+set pipe=
+rem set pipe=> %currentfolder%/run.log
+cd %runablefolder%
+%runable% %options% :path %srcfolder% %pipe%
+cd %currentfolder%
